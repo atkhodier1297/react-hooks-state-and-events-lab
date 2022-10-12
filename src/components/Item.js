63ace405748f319b1575b8239076961f62f060b7
Adam Khodier <khodieradam1297@gmail.com>
2022-10-12 17:56:12 -0400

    commit tree 761b86db7744875af78e96c858d57e9bb1d9d5e1

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+
+const [ inTheCart, setInTheCart ] = useState( false )
+
+const liClass = inTheCart ? "in-cart" : ''
+
+const buttonText = inTheCart ? "Remove" : "Add to Cart"
+
+//const buttonColor = darkModeToggle ? "remove" : "add"
+
+function toggleInTheCart() {
+  setInTheCart( !inTheCart)
+}
+
   return (
-    <li className="">
+    <li className={ liClass }>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button 
+      className="add"
+      onClick = { toggleInTheCart }
+      >
+        { buttonText }
+        </button>
     </li>
   );
 }
